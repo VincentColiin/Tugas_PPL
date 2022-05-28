0843fb6051cee141dea1f6c4f25b64e916363337
rikhoari01 <rikhoari01@gmail.com>
2022-05-28 18:23:44 +0700

    add testing

--- routes/user.js
+++ routes/user.js
@@ -3,9 +3,8 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
 
-<<<<<<< HEAD
-router.get('/', (req, res) => {
-    User.find({}).then((result) => {
+router.get('/', async (req, res) => {
+    await User.find({}).then((result) => {
         res.json(result);
     }).catch((err) => {
         res.status(500).json({
@@ -14,22 +13,8 @@ router.get('/', (req, res) => {
     })
 })
 
-router.post("/register", (req, res) => {
+router.post("/register", async (req, res) => {
   var tempUser = req.body;
-=======
-router.get('/', async(req, res) => {
-    try {
-        const userList = await User.find({});
-        res.json(userList);
-    }
-    catch {
-        res.status(418).send({message: "ada masalah koneksi ke database"})
-    }
-})
-
-router.post("/register", async(req, res) => {
-  var tempUser = req.query;
->>>>>>> 3ee4156cb5390c0123f6a4460080082a1f8de530
     if (tempUser.username != undefined && tempUser.password != undefined) {
         const newUser = new User({
             username: tempUser.username,
@@ -43,19 +28,11 @@ router.post("/register", async(req, res) => {
     }
 });
 
-<<<<<<< HEAD
-router.post("/login", (req, res) => {
+router.post("/login", async (req, res) => {
     var tempUser = req.body;
-    var username = req.body.username
-    var oldUser = User.findOne({username});
-    if (oldUser != null && oldUser.password == tempUser.password) {
-=======
-router.post("/login", async(req, res) => {
-    var password = req.query.password;
-    var username = req.query.username;
+    var username = req.body.username;
     var oldUser = await User.findOne({username});
-    if (oldUser != null && oldUser.password == password) {
->>>>>>> 3ee4156cb5390c0123f6a4460080082a1f8de530
+    if (oldUser != null && oldUser.password == tempUser.password) {
     res.status(200).send({ message: "login berhasil" });
     } else {
     res.status(418).send({ message: "username/password salah" });
--- test/user.js
+++ test/user.js
@@ -24,7 +24,7 @@ describe("User", () => {
         .get("/user")
         .end((err, res) => {
           res.should.have.status(200);
-          res.body.should.be.a('array');
+          res.body.should.be.a("array");
           res.body.length.should.be.eql(0);
           done();
         });
@@ -37,7 +37,6 @@ describe("User", () => {
         username: "user123",
         password: "12345678",
       };
-
       chai
         .request(server)
         .post("/user/register")
@@ -48,6 +47,65 @@ describe("User", () => {
           res.body.should.have.property("message").eql("User sudah tersimpan");
           done();
         });
+    });
+    it("Mendaftarkan user tanpa password", (done) => {
+      let user = {
+        username: "user123",
+      };
+      chai
+        .request(server)
+        .post("/user/register")
+        .send(user)
+        .end((err, res) => {
+          res.should.have.status(418);
+          res.body.should.be.a("object");
+          res.body.should.have
+            .property("message")
+            .eql("username/password tidak memenuhi kriteria");
+          done();
+        });
+    });
+  });
+
+  describe("/POST Login", () => {
+    it("Login dengan username dan password yang benar", (done) => {
+      let user = new User({
+        username: "user123",
+        password: "12345678",
+      });
+      user.save((err) => {
+        chai
+          .request(server)
+          .post("/user/login")
+          .send(user)
+          .end((err, res) => {
+            res.should.have.status(200);
+            res.body.should.be.a("object");
+            res.body.should.have.property("message").eql("login berhasil");
+            done();
+          });
+      });
+    });
+    it("Login dengan password yang salah", (done) => {
+      let user = new User({
+        username: "user123",
+        password: "12345678",
+      });
+      user.save((err) => {
+        chai
+          .request(server)
+          .post("/user/login")
+          .send({
+            username: "user123",
+            password: "1234567890",
+          })
+          .end((err, res) => {
+            res.should.have.status(418);
+            res.body.should.be.a("object");
+            res.body.should.have.property("message").eql("username/password salah");
+            done();
+          });
+      });
     });
   });
 });
